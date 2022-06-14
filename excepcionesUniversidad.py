792cb18f943809cca1a923ef0af6ca4d5246a625
aralrom <82242208+aralrom@users.noreply.github.com>
2022-06-14 13:04:57 +0200

    Add files via upload

--- excepcionesUniversidad.py
+++ excepcionesUniversidad.py
@@ -1,1 +1,22 @@
 # Implementar excepciones
+from excepcionesUniversidad import *
+
+
+class ExcepcionesUniversidad(Exception):
+    """Clase base de Excepciones"""
+    def __str__(self):
+        return f'{self.message}'
+
+
+class ClaseSinEstudiantes(ExcepcionesUniversidad):
+    """Raise cuando la asignatura no tiene estudiantes"""
+    def __init__(self, message="La asignatura no tiene estudiantes"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ClaseSinProfesor(ExcepcionesUniversidad):
+    """Raised cuando la asignatura no tiene profesor"""
+    def __init__(self, message="La asignatura no tiene un profesor"):
+        self.message = message
+        super().__init__(self.message)
